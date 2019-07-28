7b2dcdd56b8959a187638d15ea4cba0e1df795b8
Mohamed Samir <mohamedsamir@Mohameds-MacBook-Air.local>
2019-07-28 07:16:26 -0700

    Initial Commit

---
+++ collectionViewTechnique/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionViewTechnique
+//
+//  Created by Mohamed Samir on 7/28/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
